090d81cd875327c38222036adcf42a76071e1cf8
ishpreet <ishpreet.singh@argusoft.com>
2026-09-25 15:46:16 +0530

    Added initial migration for user and organiser

---
+++ Backend/index.js
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import {Prisma}
+import { PrismaClient } from "./generated/prisma/client.ts";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+console.log("DATABASE_URL =", process.env.DATABASE_URL);
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+
+const main = async () => {
+  const allUsers = await prisma.Users.findMany();
+  console.log(allUsers);
+};
+main()
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
